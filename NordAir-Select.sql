e39de181a5efb111ee0314f595dc1ef55bfac4fb
Arthur Tirado <arthurmegiolaro@gmail.com>
2024-04-25 21:15:34 -0400

    Commit finale

--- NordAir-Select.sql
+++ NordAir-Select.sql
@@ -72,11 +72,11 @@ SELECT
 	LEFT(VOL.NO_VOL, 10) AS VOL,
 	LEFT(MIN(V_ENVOLEES_SIEGES.NOMBRE_PLACES - V_ENVOLEES_SIEGES.NB_RESERVE), 20) AS DISPONIBLES
 FROM
-	SEGMENT
-	INNER JOIN ENVOLEE
-		ON ENVOLEE.ID_SEGMENT = SEGMENT.ID_SEGMENT
-	INNER JOIN VOL
-		ON VOL.ID_VOL = SEGMENT.ID_VOL
+	VOL
+	INNER JOIN SEGMENT ON
+		VOL.ID_VOL = SEGMENT.ID_VOL
+	INNER JOIN ENVOLEE ON
+		SEGMENT.ID_SEGMENT = ENVOLEE.ID_SEGMENT
 	INNER JOIN V_ENVOLEES_SIEGES
 		ON V_ENVOLEES_SIEGES.ID_ENVOLEE = ENVOLEE.ID_ENVOLEE
 WHERE
@@ -85,7 +85,7 @@ GROUP BY
 	ENVOLEE.DATE_ENVOLEE,
 	VOL.NO_VOL
 ORDER BY
-	ENVOLEE.DATE_ENVOLEE, 
+	ENVOLEE.DATE_ENVOLEE,
 	VOL.NO_VOL
 
 /*
@@ -168,4 +168,68 @@ ORDER BY
 	V_PASSAGERS_VOLS.NOM,
 	V_PASSAGERS_VOLS.PRENOM,
 	V_PASSAGERS_VOLS.ID_PASSAGER
-	
+/*
+DEPART     A     VOL                                     PASSAGER                                                                     DE                   A
+---------- ----- --------------------------------------- ---------------------------------------------------------------------------- -------------------- --------------------
+2024-05-13 06:00 1822                                    Barbeau, Isabelle (1)                                                        MONT-JOLI            BAIE-COMEAU
+2024-05-13 06:00 1822                                    Berthiaume, Jonny (2)                                                        MONT-JOLI            BAIE-COMEAU
+2024-05-13 06:00 1822                                    BrindAmour, Jean-Francois (4)                                                MONT-JOLI            BAIE-COMEAU
+2024-05-13 06:00 1822                                    Desjardins, Francois (5)                                                     MONT-JOLI            HAVRE SAINT-PIERRE
+2024-05-13 07:00 1822                                    Bolduc, Sebastien (3)                                                        BAIE-COMEAU          SEPT-ILES
+2024-05-13 07:00 1822                                    Mercier, David (16)                                                          BAIE-COMEAU          SEPT-ILES
+2024-05-13 20:00 1823                                    Berthiaume, Jonny (2)                                                        BAIE-COMEAU          MONT-JOLI
+2024-05-13 20:30 1922                                    Nadeau, Michel (14)                                                          GASPE                HAVRE SAINT-PIERRE
+2024-05-13 05:30 1923                                    Fortin, Mathieu (9)                                                          HAVRE SAINT-PIERRE   SEPT-ILES
+2024-05-13 05:30 1923                                    Ouellet, Remi (32)                                                           HAVRE SAINT-PIERRE   SEPT-ILES
+2024-05-13 05:30 1923                                    Gregoire, Pierre-Luc (12)                                                    HAVRE SAINT-PIERRE   BAIE-COMEAU
+2024-05-13 05:30 1923                                    Lapointe-Girard, etienne (29)                                                HAVRE SAINT-PIERRE   BAIE-COMEAU
+2024-05-13 05:30 1923                                    Ouellette, Pierre (33)                                                       HAVRE SAINT-PIERRE   BAIE-COMEAU
+2024-05-13 05:30 1923                                    Talbot, Remi (36)                                                            HAVRE SAINT-PIERRE   MONT-JOLI
+2024-05-13 05:30 1923                                    Tremblay, Sebastien (35)                                                     HAVRE SAINT-PIERRE   MONT-JOLI
+2024-05-13 07:30 1923                                    Poussier, Genevieve (20)                                                     SEPT-ILES            BAIE-COMEAU
+2024-05-13 07:30 1923                                    Tremblay, Vincent (15)                                                       SEPT-ILES            BAIE-COMEAU
+2024-05-14 09:00 1822                                    Harnois, Stephane J. (8)                                                     GASPE                HAVRE SAINT-PIERRE
+2024-05-14 06:30 1923                                    BrindAmour, Jean-Francois (4)                                                GASPE                SEPT-ILES
+2024-05-15 08:00 1822                                    Rouillard, Guy (37)                                                          SEPT-ILES            GASPE
+2024-05-15 19:30 1922                                    Fortin, Mathieu (9)                                                          SEPT-ILES            HAVRE SAINT-PIERRE
+2024-05-15 20:30 1922                                    Cote, Mathieu (6)                                                            GASPE                HAVRE SAINT-PIERRE
+2024-05-16 06:00 1822                                    Duval, Philippe (18)                                                         MONT-JOLI            BAIE-COMEAU
+2024-05-16 06:00 1822                                    Lapierre, Guillaume (19)                                                     MONT-JOLI            BAIE-COMEAU
+2024-05-16 06:00 1822                                    Mallet, Sylvain (21)                                                         MONT-JOLI            BAIE-COMEAU
+2024-05-16 06:00 1822                                    Saucier, Jean-Francois (22)                                                  MONT-JOLI            HAVRE SAINT-PIERRE
+2024-05-16 07:00 1822                                    Poussier, Genevieve (20)                                                     BAIE-COMEAU          SEPT-ILES
+2024-05-16 09:00 1822                                    Fortin, Jean-Philippe (7)                                                    GASPE                HAVRE SAINT-PIERRE
+2024-05-16 18:00 1823                                    Jobin, Samuel (10)                                                           GASPE                SEPT-ILES
+2024-05-16 18:00 1823                                    Rouillard, Guy (37)                                                          GASPE                SEPT-ILES
+2024-05-16 20:00 1823                                    Lapierre, Guillaume (19)                                                     BAIE-COMEAU          MONT-JOLI
+2024-05-16 20:30 1922                                    Aubert, Vincent (31)                                                         GASPE                HAVRE SAINT-PIERRE
+2024-05-16 05:30 1923                                    Fortin, Jean-Philippe (7)                                                    HAVRE SAINT-PIERRE   GASPE
+2024-05-16 05:30 1923                                    Fortin, Mathieu (9)                                                          HAVRE SAINT-PIERRE   GASPE
+2024-05-16 05:30 1923                                    Desjardins, Francois (5)                                                     HAVRE SAINT-PIERRE   BAIE-COMEAU
+2024-05-17 09:00 1822                                    Aspiros, Charles (25)                                                        GASPE                HAVRE SAINT-PIERRE
+2024-05-17 05:30 1923                                    Ratte, Francois (11)                                                         HAVRE SAINT-PIERRE   GASPE
+2024-05-17 06:30 1923                                    Mallet, Sylvain (21)                                                         GASPE                SEPT-ILES
+2024-05-17 07:30 1923                                    Ouellet, Remi (32)                                                           SEPT-ILES            BAIE-COMEAU
+2024-05-17 08:30 1923                                    Desjardins, Francois (5)                                                     BAIE-COMEAU          MONT-JOLI
+2024-05-18 06:00 1822                                    Rouillard, Guy (37)                                                          MONT-JOLI            BAIE-COMEAU
+2024-05-18 06:00 1822                                    Talbot, Remi (36)                                                            MONT-JOLI            BAIE-COMEAU
+2024-05-18 06:00 1822                                    Tremblay, Sebastien (35)                                                     MONT-JOLI            BAIE-COMEAU
+2024-05-18 07:00 1822                                    Gregoire, Pierre-Luc (12)                                                    BAIE-COMEAU          SEPT-ILES
+2024-05-18 07:00 1822                                    Lapointe-Girard, etienne (29)                                                BAIE-COMEAU          SEPT-ILES
+2024-05-18 07:00 1822                                    Ouellette, Pierre (33)                                                       BAIE-COMEAU          SEPT-ILES
+2024-05-18 09:00 1822                                    Lagace, Mathieu (23)                                                         GASPE                HAVRE SAINT-PIERRE
+2024-05-18 09:00 1822                                    Ratte, Francois (11)                                                         GASPE                HAVRE SAINT-PIERRE
+2024-05-18 19:00 1823                                    Dostie-Proulx, Pierre-Luc (13)                                               SEPT-ILES            BAIE-COMEAU
+2024-05-18 17:30 1922                                    Dionne, Marjorie (17)                                                        MONT-JOLI            BAIE-COMEAU
+2024-05-18 17:30 1922                                    Pelletier, Nathalie (34)                                                     MONT-JOLI            BAIE-COMEAU
+2024-05-18 19:30 1922                                    Picard, Maxime (26)                                                          SEPT-ILES            HAVRE SAINT-PIERRE
+2024-05-19 09:00 1822                                    Blanchette, Marc (24)                                                        GASPE                HAVRE SAINT-PIERRE
+2024-05-19 18:00 1823                                    Richard, Jean-Francois (27)                                                  GASPE                SEPT-ILES
+2024-05-19 19:00 1823                                    Mercier, Nicolas (30)                                                        SEPT-ILES            BAIE-COMEAU
+2024-05-19 05:30 1923                                    Blanchette, Marc (24)                                                        HAVRE SAINT-PIERRE   GASPE
+2024-05-19 05:30 1923                                    Dube, Jason (28)                                                             HAVRE SAINT-PIERRE   GASPE
+2024-05-19 05:30 1923                                    Picard, Maxime (26)                                                          HAVRE SAINT-PIERRE   GASPE
+2024-05-19 05:30 1923                                    Saucier, Jean-Francois (22)                                                  HAVRE SAINT-PIERRE   BAIE-COMEAU
+
+(59 rows affected)
+*/
