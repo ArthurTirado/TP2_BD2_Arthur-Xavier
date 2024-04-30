a515d702cc5ce98a2426ab4a4468407ea5be2f50
Xavbl <2237907@csfoy.ca>
2024-04-30 10:37:28 -0400

    e1 MINUTES_VOL_PILOTE

--- NordAir-T-SQL E1.sql
+++ NordAir-T-SQL E1.sql
@@ -1,5 +1,40 @@
 /* **********************************************************
-	NordAir - Transact SQL 1
+	NordAir - Transact SQL E1
 	Schéma MRD:	"NordAir"
 	Auteur:		Arthur Tirado et Xavier Breton-L'italien	
 ********************************************************** */
+
+USE NORDAIR
+GO
+
+CREATE OR ALTER FUNCTION MINUTES_EN_HEURES(@minutes SMALLINT) RETURNS DECIMAL(9,3)
+AS
+BEGIN
+	DECLARE @heures DECIMAL(9,3)
+	DECLARE @minutesRestantes DECIMAL(9,3)
+	SET @heures = @minutes / 60
+	SET @minutesRestantes = (@minutes - (@heures * 60)) / 60
+	SET @heures += @minutesRestantes
+	RETURN @heures
+END
+GO
+
+CREATE OR ALTER FUNCTION MINUTES_VOL_PILOTE(@no_pilote SMALLINT, @date1 DATE, @date2 DATE) RETURNS SMALLINT
+AS
+BEGIN
+	DECLARE @minutesVol SMALLINT
+
+	SET @minutesVol =
+	(SELECT
+		SUM(SEGMENT.DUREE_VOL)
+	FROM
+		PILOTE
+		INNER JOIN ENVOLEE
+			ON ENVOLEE.ID_PILOTE = PILOTE.ID_PILOTE
+		INNER JOIN SEGMENT
+			ON SEGMENT.ID_SEGMENT = ENVOLEE.ID_SEGMENT
+	WHERE
+		PILOTE.NO_PILOTE = @no_pilote AND ENVOLEE.DATE_ENVOLEE BETWEEN @date1 AND @date2)
+
+	RETURN @minutesVol
+END
